3bbc312082cb7db56587bdbd598c61270ebf0da8
Wendel Davi <149116208+Wendel-Costa@users.noreply.github.com>
2025-12-31 20:34:55 +0000

    criando a classe usuario

---
+++ src/sistemacursos/model/usuarios/Usuario.java
@@ -0,0 +1,15 @@
+package sistemacursos.model.usuarios;
+
+public abstract class Usuario {
+    protected int id;
+    protected String nome;
+    protected String email;
+
+    public Usuario(int id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public abstract String getUsuario();
+}
